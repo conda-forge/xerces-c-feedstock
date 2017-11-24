ba620df12d42f12b6b49160ddc51c98046aa9e75
Filipe Fernandes <ocefpaf@gmail.com>
2017-11-24 11:49:37 -0200

    try to find the ver

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,10 +1,12 @@
 import os
 import sys
 import ctypes
-import platform
+
+version = os.getenv("PKG_VERSION")
+vstring = '_'.join(version.split('.')[:2])
 
 if sys.platform == 'win32':
-    lib = ctypes.CDLL('xerces-c_3_1.dll')
+    lib = ctypes.CDLL('xerces-c_%s.dll' % vstring)
 elif sys.platform == 'darwin':
     # LD_LIBRARY_PATH not set on OSX or Linux
     path = os.path.expandvars('$PREFIX/lib/libxerces-c.dylib')
