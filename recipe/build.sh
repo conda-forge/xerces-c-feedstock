b012f97520d4c1ac4f56d1d0cfa904562ba0666b
Filipe Fernandes <ocefpaf@gmail.com>
2018-09-03 10:10:17 -0300

    remove .la

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,9 @@ then
 fi
 
 ./configure --prefix=$PREFIX --with-icu --disable-network
-make
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
+
+# We can remove this when we start using the new conda-build.
+find $PREFIX -name '*.la' -delete
