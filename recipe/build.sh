9550b9db02c3d25c0d5389e0acb4bffdfcb657e7
Filipe Fernandes <ocefpaf@gmail.com>
2018-10-17 13:52:38 -0300

    update to 3.2.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-if [ "$(uname)" == "Darwin" ]
-then
-  export CXX="${CXX} -stdlib=libc++"
-fi
-
 ./configure --prefix=$PREFIX --with-icu --disable-network
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
-
-# We can remove this when we start using the new conda-build.
-find $PREFIX -name '*.la' -delete
