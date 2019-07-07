40fe3d8d3a4cc56c751e5a5ea8e066fe99c57dd6
Filipe Fernandes <ocefpaf@gmail.com>
2019-07-07 15:58:07 -0500

    remove static lib

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX --with-icu
+./configure --prefix=${PREFIX} \
+            --with-icu \
+            --enable-static=no
+
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
