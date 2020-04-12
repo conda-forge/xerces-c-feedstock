bc6da00df8e323c40e45303eec9cd5efab240fac
Sam Gillingham <gillingham.sam@gmail.com>
2020-04-12 14:21:43 +1000

    attempt to fix Windows error

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -6,7 +6,8 @@ version = os.getenv("PKG_VERSION")
 vstring = '_'.join(version.split('.')[:2])
 
 if sys.platform == 'win32':
-    lib = ctypes.CDLL('xerces-c_%s.dll' % vstring)
+    path = os.path.join(sys.prefix, 'Library', 'bin', 'xerces-c_%s.dll' % vstring)
+    lib = ctypes.CDLL(path)
 elif sys.platform == 'darwin':
     # LD_LIBRARY_PATH not set on OSX or Linux
     path = os.path.expandvars('$PREFIX/lib/libxerces-c.dylib')
