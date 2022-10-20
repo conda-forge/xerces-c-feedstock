44ed23d12c07176b1444a4e0fbbe76dafb10d754
Marie-Terrell <marie.terrell@cfa.harvard.edu>
2022-10-20 15:25:24 -0400

    Update build number and add libcurl dep

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,8 @@ fi
 
 ./configure --prefix=${PREFIX} \
             --with-icu \
-            --enable-static=no
+            --with-curl=${PREFIX} \
+	    --enable-static=no
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
